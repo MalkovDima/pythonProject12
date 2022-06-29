0261e4868cbb52a64e8641c9b05ecc5c0335f489
Malkov Dima <malkovich.23@hotmail.com>
2022-06-29 14:41:02 +0500

    add json fail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 from datetime import date
+import json
 
 
 class FotoVk:
@@ -13,7 +14,7 @@ class FotoVk:
         }
 
     # Создание словаря, с именем и url фотографий для копирования
-    def get_foto(self, number='5'):
+    def get_foto(self, number=5):
         url_photo_get = self.url + 'photos.get'
         params_photo_get = {
             'owner_id': '263903',
@@ -23,14 +24,22 @@ class FotoVk:
             'count': number
         }
         new_dict = {}
+        new_list = []
         res = requests.get(url_photo_get, params={**self.params, **params_photo_get}).json()
         for n in res['response']['items']:
+            new_dict_for_json = {}
             if not n['likes']['count'] in new_dict:
                 new_dict[n['likes']['count']] = n['sizes'][len(n['sizes']) - 1]['url']
+                new_dict_for_json['file_name'] = str(n['likes']['count'])
+                new_dict_for_json['size'] = str(n['sizes'][len(n['sizes']) - 1]['type'])
             else:
                 new_dict[str(n['likes']['count']) + '-' + str(n['date'])] = n['sizes'][len(n['sizes']) - 1]['url']
+                new_dict_for_json['file_name'] = str(n['likes']['count']) + '-' + str(n['date'])
+                new_dict_for_json['size'] = str(n['sizes'][len(n['sizes']) - 1]['type'])
+            new_list.append(new_dict_for_json)
+        ret = [new_dict, new_list]
         pprint(f'выбрано {number} фотографий для резервного копирования!')
-        return new_dict
+        return ret
 
 
 class YaUploader:
@@ -77,8 +86,10 @@ if __name__ == '__main__':
                    '-fgrrDWtyp3avyL '
     version = '5.131'
     pf = FotoVk(you_token_vk, version)
-    dict_foto = pf.get_foto()
+    list_foto = pf.get_foto(4)
     name_folder = str(date.today())
+    with open("json_foto.json", 'w') as fout:
+                 json.dump(list_foto[1], fout, indent=2)
     you_token_ya = 'AQAAAABiV6hBAADLW0MczaVdtUkUvEAkcVVYNj8'
     uploader = YaUploader(you_token_ya)
-    uploader.save_dict_foto(dict_foto, name_folder)
+    uploader.save_dict_foto(list_foto[0], name_folder)
